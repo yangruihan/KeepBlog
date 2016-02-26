cf000e2296d32278910fd50e17e8a62b4ce7c13d
C.y <yangruihan@vip.qq.com>
2016-02-27 00:55:45 +0800

    完成注册页及其基本功能

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'myblog'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^do_register/$', views.do_register, name='do_register'),
+    url(r'^register_suc/$', views.register_suc, name='register_suc'),
+    url(r'^register_fail/$', views.register_fail, name='register_fail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+
+from .models import User
 
 def index(request):
     return render(request, 'myblog/index.html')
+
+def register(request):
+    return render(request, 'myblog/register.html')
+
+def register_suc(request):
+    return render(request, 'myblog/register_suc.html')
+
+def register_fail(request):
+    return render(request, 'myblog/register_fail.html')
+
+def do_register(request):
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User(username=username, password=password, email=email, create_date=timezone.now())
+        user.save()
+    except KeyError as e:
+        # return render(request, 'myblog/register.html', {
+        #         'error_message': u"你必须将信息填写完整",
+        #     })
+        print(e)
+        return HttpResponseRedirect(reverse('myblog:register_fail'))
+    else:
+        return HttpResponseRedirect(reverse('myblog:register_suc'))
